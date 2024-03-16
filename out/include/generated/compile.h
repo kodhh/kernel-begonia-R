04f4cfdaface0929910ab3ef620de03b0dc4b76c
kodhh <limw94@outlook.com>
2024-03-16 18:16:29 +0800

    KSU

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Mar 16 18:15:54 CST 2024"
+#define LINUX_COMPILE_BY "li"
+#define LINUX_COMPILE_HOST "li-W650KL"
+#define LINUX_COMPILER "Ubuntu clang version 12.0.1-19ubuntu3, Ubuntu LLD 12.0.1"
